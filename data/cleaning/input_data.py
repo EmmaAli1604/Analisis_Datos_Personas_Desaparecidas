b2f4d4e1cbce7761c2e933019a4f2087419adcca
EmmaAli1604 <emma_jimenez1601@ciencias.unam.mx>
2026-03-24 21:46:25 -0600

    feat(input_data): stadistic input dataset

--- data/cleaning/input_data.py
+++ data/cleaning/input_data.py
@@ -1,28 +1,59 @@
 from ..config.config import DATA_PROCESSED
 import pandas as pd
 
-def input_data_knn(df):
-    pass
-
 def input_data_mean(df, columns):
-    pass
+    for col in columns:
+        df[col] = df[col].fillna(df[col].mean())
+    return df
 
 def input_data_median(df, columns):
-    pass
+    for col in columns:
+        df[col] = df[col].fillna(df[col].median())
+    return df
 
 def input_data_mode(df, columns):
-    pass
+    for col in columns:
+        df[col] = df[col].fillna(df[col].mode()[0])
+    return df
+
+def limpiar_fecha(x):
+    if pd.isna(x):
+        return pd.NaT
+    try:
+        return pd.to_datetime(x, errors='coerce')
+    except:
+        return pd.NaT
 
 def main():
-    df = pd.read_csv(DATA_PROCESSED)
-    df_knn = input_data_knn(df)
-    df_knn.to_csv('data/input_data/data_knn.csv', index=False)
-    df_mean = input_data_mean(df, ['FECHA_NACIMIENTO', 'FECHA_DESAPARICION', 'FECHA_REGISTRO'])
+    df_processed = pd.read_csv(DATA_PROCESSED)
+    
+    # Normalizar las fechas
+    
+    cols_fecha = ['FECHA_NACIMIENTO', 'FECHA_DESAPARICION', 'FECHA_REGISTRO']
+
+    for col in cols_fecha:
+        df_processed[col] = df_processed[col].apply(lambda x: limpiar_fecha(x))
+    
+    # Input data con diferentes técnicas de imputación
+    df_mean = input_data_mean(df_processed, ['FECHA_NACIMIENTO', 'FECHA_DESAPARICION', 'FECHA_REGISTRO'])
     df_mean.to_csv('data/input_data/data_mean.csv', index=False)
-    df_median = input_data_median(df, ['FECHA_NACIMIENTO', 'FECHA_DESAPARICION', 'FECHA_REGISTRO'])
+    df_median = input_data_median(df_processed, ['FECHA_NACIMIENTO', 'FECHA_DESAPARICION', 'FECHA_REGISTRO'])
     df_median.to_csv('data/input_data/data_median.csv', index=False)
-    df_mode = input_data_mode(df, ['FECHA_NACIMIENTO', 'FECHA_DESAPARICION', 'FECHA_REGISTRO'])
+    df_mode = input_data_mode(df_processed, ['FECHA_NACIMIENTO', 'FECHA_DESAPARICION', 'FECHA_REGISTRO'])
+    
+    # Guardar los DataFrames con los datos imputados con estadistica simple
     df_mode.to_csv('data/input_data/data_mode.csv', index=False)
+    df_mean.to_csv('data/input_data/data_mean.csv', index=False)
+    df_median.to_csv('data/input_data/data_median.csv', index=False)
+    
+    # Datos imputados para analisis
+    df_processed = input_data_mean(df_processed, ['FECHA_NACIMIENTO'])
+    # Imputar usando la mediana de la ENTIDAD federativa
+    df_processed['FECHA_DESAPARICION'] = df_processed.groupby('ENTIDAD')['FECHA_DESAPARICION'].transform(
+        lambda x: x.fillna(x.median())
+    )
+    df_processed['FECHA_REGISTRO'] = df_processed['FECHA_REGISTRO'].fillna(df_processed['FECHA_DESAPARICION'])
+    df_processed.to_csv('data/input_data/data_imputed.csv', index=False)
 
 if __name__ == "__main__":
     main()
--- data/config/config.py
+++ data/config/config.py
@@ -1,2 +1,2 @@
 DATA_RAW = 'data/raw/data_secretariado.csv'
-DATA_PROCESSED = 'data/processed/data_secretariado.csv'
+DATA_PROCESSED = 'data/processed/data_processed.csv'
