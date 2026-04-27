74896a5ecaced52c37a0ec73df029772c37b0025
EmmaAli1604 <emma_jimenez1601@ciencias.unam.mx>
2026-04-26 22:57:25 -0600

    feat(models): adding models files

---
+++ main.py
@@ -0,0 +1,7 @@
+from .streamlit.app import main as streamlit_main
+
+def main():
+    streamlit_main()
+
+if __name__ == "__main__":
+    main()
---
+++ src/models/association-rules.py
@@ -0,0 +1,6 @@
+def main():
+    print("This is the association rules model.")
+    pass
+
+if __name__ == "__main__":
+    main()
