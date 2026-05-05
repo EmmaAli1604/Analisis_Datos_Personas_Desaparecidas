11035d7a2082491493735fb12f5d3cf8354b86d0
EmmaAli1604 <emma_jimenez1601@ciencias.unam.mx>
2026-05-05 00:14:21 -0600

    simulador umbral fic

--- pages/simulador_umbral.py
+++ pages/simulador_umbral.py
@@ -2,7 +2,7 @@ from components.simulador_umbral import simulador_umbral
 from src.models.forecasting import main_forecasting
 
 def page_simulador_umbral():
-    y_test, y_predictions, model, X_test, data_raw = main_forecasting()
+    y_test, y_predictions, model, X_test, data_raw, X_test_scaled, y_probs = main_forecasting()
     simulador_umbral(model, X_test, y_test)
 
 page_simulador_umbral()
